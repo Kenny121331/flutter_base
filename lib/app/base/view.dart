48ad43f6791614042dc4a5318d29d76ff364646f
manhnt <manhnt@lifetimetech.vn>
2020-10-15 15:04:48 +0700

    first commit

---
+++ lib/app/base/view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_base/app/base/controller.dart';
+import 'package:get/get.dart';
+
+abstract class View<T extends Controller> extends GetView<T> {
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(child: buildPage(), onWillPop: willPopCallBack);
+  }
+
+  Widget buildPage();
+
+  Future<bool> willPopCallBack() async {
+
+    return true;
+  }
+}
